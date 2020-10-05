38ef49fef70b4d33cc7b02325d1277a8bfd5d737
Anand Singh <anandsingh3068@gmail.com>
2020-10-05 15:26:32 +0530

    Added LCS in BST

---
+++ C++/LCS_in_a_BST.cpp
@@ -0,0 +1,16 @@
+//Finds Least Common Ancestor of a BST
+
+Node* LCA(Node *root, int n1, int n2)
+{
+ if(root==NULL)
+ return NULL;
+ 
+ if(n1<root->data&&n2<root->data){
+   return LCA(root->left,n1,n2);  
+ }
+  if(n1>root->data&&n2>root->data){
+    return LCA(root->right,n1,n2);
+ }
+
+ return root;
+}
