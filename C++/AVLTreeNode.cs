6d3420d0e62dbc9f13541a527758ab8ac855dbf4
sanskar srivastava <59871104+sanskarsri@users.noreply.github.com>
2020-10-06 00:29:46 +0530

    Binary Search tree code added in c++

---
+++ C++/AVLTreeNode.cs
@@ -0,0 +1,53 @@
+﻿namespace DSA.DataStructures.Trees
+{
+    /// <summary>
+    /// Represents a node in the <see cref="AVLTree{T}"/>.
+    /// </summary>
+    public class AVLTreeNode<T> : BinarySearchTreeNode<T>
+    {
+        /// <summary>
+        /// Gets the left child of the <see cref="AVLTreeNode{T}"/>.
+        /// </summary>
+        public new AVLTreeNode<T> Left
+        {
+            get { return (AVLTreeNode<T>)base.Left; }
+            internal set { base.Left = value; }
+        }
+        /// <summary>
+        /// Gets the right child of the <see cref="AVLTreeNode{T}"/>.
+        /// </summary>
+        public new AVLTreeNode<T> Right
+        {
+            get { return (AVLTreeNode<T>)base.Right; }
+            internal set { base.Right = value; }
+        }
+
+        /// <summary>
+        /// Gets the value contained in the <see cref="AVLTreeNode{T}"/>.
+        /// </summary>
+        public override T Value { get; internal set; }
+
+        /// <summary>
+        /// Gets the height of the <see cref="AVLTreeNode{T}"/>.
+        /// </summary>
+        public int Height { get; internal set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AVLTreeNode{T}"/> class, containing the specified value.
+        /// </summary>
+        /// <param name="value">The value to contain in the <see cref="AVLTreeNode{T}"/>.</param>
+        public AVLTreeNode(T value) : base(value)
+        {
+            Value = value;
+            Height = 1;
+        }
+
+        /// <summary>
+        /// Removes all references the <see cref="AVLTreeNode{T}"/> has.
+        /// </summary>
+        internal override void Invalidate()
+        {
+            base.Invalidate();
+        }
+    }
+}
---
+++ C++/BinarySearchTreeNode.cs
@@ -0,0 +1,41 @@
+﻿namespace DSA.DataStructures.Trees
+{
+    /// <summary>
+    /// Represents a node in the <see cref="BinarySearchTree{T}"/>.
+    /// </summary>
+    public class BinarySearchTreeNode<T>
+    {
+        /// <summary>
+        /// Gets the left child of the <see cref="BinarySearchTreeNode{T}"/>.
+        /// </summary>
+        public BinarySearchTreeNode<T> Left { get; internal set; }
+
+        /// <summary>
+        /// Gets the right child of the <see cref="BinarySearchTreeNode{T}"/>.
+        /// </summary>
+        public BinarySearchTreeNode<T> Right { get; internal set; }
+
+        /// <summary>
+        /// Gets the value contained in the <see cref="BinarySearchTreeNode{T}"/>.
+        /// </summary>
+        public virtual T Value { get; internal set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BinarySearchTreeNode{T}"/> class, containing the specified value.
+        /// </summary>
+        /// <param name="value">The value to contain in the <see cref="BinarySearchTreeNode{T}"/>.</param>
+        public BinarySearchTreeNode(T value)
+        {
+            Value = value;
+        }
+
+        /// <summary>
+        /// Removes all references the <see cref="BinarySearchTreeNode{T}"/> has.
+        /// </summary>
+        internal virtual void Invalidate()
+        {
+            Left = null;
+            Right = null;
+        }
+    }
+}
