8563224c452c549c4f8e691fcf17eb54f04f84fc
swoyamsss <64949434+swoyamsss@users.noreply.github.com>
2020-10-05 16:19:28 +0530

    Update Diagonal view of a tree.cpp

--- C++/Diagonal view of a tree.cpp
+++ C++/Diagonal view of a tree.cpp
@@ -2,6 +2,18 @@
 using namespace std;
 
 // Author: Swoyam S Sahoo
+//diagonal view of a binary tree means the nodes of the tree we encounter when we mode from one courner to other diagonally
+//example - let our tree be       1
+ //                            /     \
+//                            2        3
+//                          /   \    /   \
+//                         4    5   6     7
+//                                    \
+//                                     8           
+//let we are moving from left left most cornor to rightmost diagonally
+// in this cast the output should be     -      4
+//                                              2 5 6 8
+//                                              1 3 7
 
 struct Node
 {
@@ -18,12 +30,12 @@ struct Node *newnode(int x)
 
     return temp;
 }
-void printtopbuttom(struct Node *root)
+void diagonalview(struct Node *root)
 {
     int k;
-    queue<struct Node *> q1;
-    queue<int> q2;
-    map<int, list<struct Node *>> mp;
+    queue<struct Node *> q1;//for temporarily storing the nodes
+    queue<int> q2;//to store temporary numbers corresponding to the nodes stord in q1
+    map<int, list<struct Node *>> mp; //to store all the nodes according to the number in q2
     q1.push(root);
     q2.push(0);
     while (!q1.empty())
@@ -50,7 +62,7 @@ void printtopbuttom(struct Node *root)
     {
         while (!i.second.empty())
         {
-            cout << i.second.front()->data << " ";
+            cout << i.second.front()->data << " ";//we are moving from front to back and eliminating simultaniously
             i.second.pop_front();
         }
         cout << endl;
@@ -67,6 +79,6 @@ int main()
     root->right->left = newnode(6);
     root->right->right = newnode(7);
     root->right->left->right = newnode(8);
-    printtopbuttom(root);
+    diagonalview(root);
     return 0;
 }
