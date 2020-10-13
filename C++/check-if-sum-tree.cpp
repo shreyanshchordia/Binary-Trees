b160f753d997c39464b70c221066afc63767ab79
shivanipawar00 <50794721+shivanipawar00@users.noreply.github.com>
2020-10-13 22:57:33 +0530

    Added check-if-sum-tree.cpp
    Problem Statement: Given a Binary Tree, find if a tree is a sum tree or not.
    Solution Approach: I have computed the sum of the root-node's left and right subtrees , and compared it with the root-node's value.They should be equal in a sum tree.
                      If they are we recurse further or we return 0; We return 1 , if the root-node is a leaf node or a nullptr;

---
+++ C++/check-if-sum-tree.cpp
@@ -0,0 +1,41 @@
+//Problem Statement: Given a Binary Tree, find if a tree is a sum tree or not.
+//Solution Approach: I have computed the sum of the root-node's left and right subtrees , and compared it with the root-node's value.They should be equal in a sum tree.
+//                   If they are we recurse further or we return 0; We return 1 , if the root-node is a leaf node or a nullptr;
+
+/* A utility function to get the sum of values in tree with root
+  as root */
+int sum(TreeNode* root)
+{
+   if(root == NULL)
+     return 0;
+   return sum(root->left) + root->data + sum(root->right);
+}
+
+
+
+/* returns 1 if sum property holds for the given
+    node and both of its children */
+
+int issumtree(TreeNode* root)
+{
+    int ls, rs;
+
+    /* If root is null or it's a leaf node then
+       return true */
+    if(node == nullptr ||
+            (node->left == null && node->right == null))
+        return 1;
+
+   /* Get sum of nodes in left and right subtrees */
+   ls = sum(root->left);
+   rs = sum(root->right);
+
+   /* if the node and both of its children satisfy the
+       property return 1 else 0*/
+    if((root->data == ls + rs)&&
+            issumtree(node->left) &&
+            issumtree(node->right))
+        return 1;
+
+   return 0;
+}
